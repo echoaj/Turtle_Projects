1c406453d5449bc669c101b2476c7b9afd6ac5ad
echoaj <aljoslin13@yahoo.com>
2020-01-17 00:28:23 -0800

    Made it into a hexagon.

--- design.py
+++ design.py
@@ -1,10 +1,11 @@
 import turtle as t
 t.speed(0)
+t.color("blue")
 
 for i in range(25):
-    for i in range(4):
+    for j in range(6):
         t.forward(100)
-        t.right(90)
+        t.right(60)
     t.right(15)
 
 
